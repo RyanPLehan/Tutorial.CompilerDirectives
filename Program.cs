2b7ac9f27ef9e946e861a2d95bd039dbdebcd9d3
Ryan Lehan <RLehan@tql.com>
2023-03-29 15:20:08 -0400

    Initial Push

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿#define MYCUSTOM_2
+
+using System;
+
+
+namespace Tutorial.CompilerDirectives
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Examples of using Compiler Directives
+            ConditionalDirective_DebugVsRelease();
+            ConditionalDirective_CustomDefinition();
+            StaticRoutines.ConditionalDirective_CustomDefinition();
+
+            ContinueAfterKeyPress();
+        }
+
+        private static void ContinueAfterKeyPress()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+
+
+
+        private static void ConditionalDirective_DebugVsRelease()
+        {
+#if DEBUG
+            Console.WriteLine("Debug Mode");
+#else
+            Console.WriteLine("Release Mode");
+#endif
+        }
+
+
+        private static void ConditionalDirective_CustomDefinition()
+        {
+#if MYCUSTOM_1
+            Console.WriteLine("Using MYCUSTOM_1 definition from Program");
+#endif
+
+#if MYCUSTOM_2
+            Console.WriteLine("Using MYCUSTOM_2 definition from Program");
+#endif
+
+        }
+
+
+    }
+}
---
+++ StaticRoutines.cs
@@ -0,0 +1,19 @@
+﻿//#define MYCUSTOM_2
+using System;
+
+namespace Tutorial.CompilerDirectives
+{
+    internal class StaticRoutines
+    {
+        public static void ConditionalDirective_CustomDefinition()
+        {
+#if MYCUSTOM_1
+            Console.WriteLine("Using MYCUSTOM_1 definition from StaticRoutines");
+#endif
+
+#if MYCUSTOM_2
+            Console.WriteLine("Using MYCUSTOM_2 definition from StaticRoutines");
+#endif
+        }
+    }
+}
